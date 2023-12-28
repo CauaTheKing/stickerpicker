eeecf8556dd8fe11aa6aefb6d28038207776ca3e
falconyklayil <cauascp37@gmail.com>
2023-12-28 13:35:06 -0300

    more stickers 3

--- sticker/version.py
+++ sticker/version.py
@@ -1,6 +1,6 @@
 # Generated in setup.py
 
 git_tag = None
-git_revision = '1683fa3c'
-version = '0.1.0+dev.1683fa3c'
-linkified_version = '0.1.0+dev.[1683fa3c](https://github.com/maunium/stickerpicker/commit/1683fa3c6945561a255bbaf6dfa35208b6878f1e)'
+git_revision = 'bd31587c'
+version = '0.1.0+dev.bd31587c'
+linkified_version = '0.1.0+dev.[bd31587c](https://github.com/maunium/stickerpicker/commit/bd31587c961cefdfc909ad6737b9bf48e01fa00c)'
